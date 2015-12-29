659bc0f4cffdc43dcce3ba96f71c40e30f5897ad
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-29 15:48:05 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
+var _ = require('lodash'),
+    GoogleMapsAPI = require('googlemaps'),
+    util = require('./util.js');
+
+var pickInputs = {
+        'origin': { key: 'origin', req: true },
+        'destination': { key: 'destination', req: true },
+        'avoid': 'avoid',
+        'language': 'language',
+        'units': 'units',
+        'traffic_model': 'traffic_model',
+        'transit_mode': 'transit_mode',
+        'transit_routing_preference': 'transit_routing_preference'
+    },
+
+    pickOutputs = {
+        'summary': { keyName: 'routes', fields: ['summary'] },
+        'warnings': { keyName: 'routes', fields: ['warnings'] },
+        'fare': { keyName: 'routes', fields: ['fare'] },
+        'legs': { keyName: 'routes', fields: ['legs'] }
+    };
+
 module.exports = {
+
+    /**
+     * Get auth data.
+     *
+     * @param step
+     * @param dexter
+     * @returns {*}
+     */
+    authOptions: function (step, dexter) {
+        var authData = {};
+
+        if (dexter.environment('google_server_key')) {
+            authData.key = dexter.environment('google_server_key');
+        } else if (dexter.environment('google_client_id') && dexter.environment('google_private_key')) {
+            authData.google_client_id = dexter.environment('google_client_id');
+            authData.google_private_key = dexter.environment('google_private_key');
+        }
+
+        return _.isEmpty(authData)? false : authData;
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +49,16 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var auth = this.authOptions(step, dexter);
+        if (!auth)
+            return this.fail('A [google_server_key] or [google_client_id,google_private_key] environment variable need for this module.');
+
+        var gmAPI = new GoogleMapsAPI(auth);
+        gmAPI.directions(util.pickInputs(step, pickInputs), function(err, result){
+            if (err)
+                this.fail(err);
+            else
+                this.complete(util.pickOutputs(result, pickOutputs));
+        }.bind(this));
     }
 };
